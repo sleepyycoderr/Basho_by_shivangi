b65e547382c0bc20a666ef762d38c77dc65c800d
sleepyycoderr <sleepyycoderr@gmail.com>
2026-01-06 14:47:34 +0530

    modified settings.py

--- backend/basho_backend/basho_backend/settings.py
+++ backend/basho_backend/basho_backend/settings.py
@@ -10,15 +10,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 from pathlib import Path
+from dotenv import load_dotenv
+ 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
