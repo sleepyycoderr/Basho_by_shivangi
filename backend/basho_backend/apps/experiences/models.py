939c69d8f008d8c3f5baa735e409e3f180ad7e02
sleepyycoderr <sleepyycoderr@gmail.com>
2026-01-13 00:20:56 +0530

    typing error

--- backend/basho_backend/apps/experiences/models.py
+++ backend/basho_backend/apps/experiences/models.py
@@ -69,10 +69,13 @@ class Booking(models.Model):
     )
 
     slot = models.ForeignKey(
-        ExperienceSlot,
-        on_delete=models.PROTECT,
-        related_name="bookings"
-    )
+    ExperienceSlot,
+    on_delete=models.PROTECT,
+    related_name="bookings",
+    null=True,
+    blank=True,
+)
+
 
     full_name = models.CharField(max_length=100)
     phone = models.CharField(max_length=15)
